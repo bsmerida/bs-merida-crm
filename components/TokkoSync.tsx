a6cc70d724698f0a178ec67d439b1ff9bf7e04ad
bsmerida <jpflores@inmobsmerida.com.mx>
2026-05-06 22:17:41 -0600

    Update TokkoSync.tsx
    se añadio correcion de error al principio

--- components/TokkoSync.tsx
+++ components/TokkoSync.tsx
@@ -1,5 +1,7 @@
 "use client";
-import ...
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 
